ceb880eb60e3327a75f54dc34a146360ce516ae7
Aleksandr <kuzovkov.aleksandr@rambler.ru>
2026-03-29 14:18:41 +0300

    ...

---
+++ todo-react/src/components/AddTaskForm.jsx
@@ -0,0 +1,17 @@
+import Field from "./Field"
+import Button from "./Button"
+
+const AddTaskForm = () => {
+  return (
+    <form className="todo__form">
+      <Field 
+      className="todo__field"
+      id="new-task"
+      label="New task title"
+      />
+      <Button />
+    </form>
+  );
+};
+
+export default AddTaskForm;
---
+++ todo-react/src/components/SearchTaskForm.jsx
@@ -0,0 +1,15 @@
+import Field from "./Field";
+const SearchTaskForm = () => {
+  return (
+    <form className="todo__form">
+        <Field 
+          className="todo__field"
+          id="search-task"
+          label="Search task"
+          type="search"
+        />
+      </form>
+  );
+};
+
+export default SearchTaskForm;
