a884761f26540c5e13c466f9460e321078cd4723
Aleksandr <kuzovkov.aleksandr@rambler.ru>
2026-04-03 16:24:28 +0300

    lesson 18

---
+++ todo-react7/readme.md
@@ -0,0 +1,40 @@
+## Create react app
+https://www.youtube.com/playlist?list=PL0MUAHwery4omH4GyVQ-lI2R326tOdN7A
+
+
+```bash
+npm create vite@7.0.3
+Need to install the following packages:
+create-vite@7.0.3
+Ok to proceed? (y) y
+
+
+> npx
+> cva
+
+│
+◇  Project name:
+│  .
+│
+◇  Current directory is not empty. Please choose how to proceed:
+│  Ignore files and continue
+│
+◇  Select a framework:
+│  React
+│
+◇  Select a variant:
+│  JavaScript
+│
+◇  Scaffolding project in /home/user1/projects/reactlearn/todo-react...
+│
+└  Done. Now run:
+
+  npm install
+  npm run dev
+
+```
+
+Начальная структура
+
+https://github.com/aleksanderlamkov/todo-react/tree/8467ef54dd9802b3443a4a7ea89f0654636c1a68
+
---
+++ todo-react7/src/entities/todo/model/taskContext.jsx
@@ -0,0 +1,52 @@
+import { createContext } from "react";
+import useTasks from "./useTasks";
+import useIncompleteTaskScroll from "./useIncompleteTaskScroll";
+
+export const TaskContext = createContext({});
+
+export const TasksProvider = (props) =>{
+  const {children} = props;
+
+  const {
+    tasks, 
+    filteredTasks,
+    deleteTask,
+    deleteAllTasks,
+    toggleTaskDone,
+    addTask,  
+    newTaskTitle,
+    setNewTaskTitle,
+    searchQuery,
+    setSearchQuery,
+    newTaskInputRef,
+    disappearingTaskId,
+    appearingTaskId,
+  } = useTasks();
+
+  const { 
+    firstIncompleteTaskRef, 
+    firstIncompleteTaskId 
+  } = useIncompleteTaskScroll(tasks);
+
+  return (
+    <TaskContext.Provider value={{
+      tasks, 
+      filteredTasks,
+      firstIncompleteTaskRef,
+      firstIncompleteTaskId,
+      deleteTask,
+      deleteAllTasks,
+      toggleTaskDone,
+      addTask,  
+      newTaskTitle,
+      setNewTaskTitle,
+      searchQuery,
+      setSearchQuery,
+      newTaskInputRef,
+      disappearingTaskId,
+      appearingTaskId,
+    }}>
+      {children}
+    </TaskContext.Provider>
+  );
+};
